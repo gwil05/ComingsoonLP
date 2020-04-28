f42620dbc7d7474ccf78f8377b2ff6893d209802
gwil05 <54655467+gwil05@users.noreply.github.com>
2020-04-28 11:43:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,27 @@ const email = document.querySelector("#email");
 const notify = document.querySelector("#btn");
 const form = document.querySelector("#form");
 
+
+//on submit listene for notify button 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
 
+    //email validation 
  if(email.value === '') {
      msg.classList.add('error');
      email.style.border = '1 px solid red';
      msg.innerHTML = 'Whoops! It looks like you forgot to add your email';
 
+     //removal of error or successs msg
      setTimeout(() => msg.remove(), 3000);
  }
 
  email.value = '';
 }
 
- 
+ ///regex for email address
 var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function checkEmail(){
